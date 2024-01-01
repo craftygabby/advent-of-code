e15424d3dc6a81c6453a09254994c5f112c700a8
Gabrielle Moran <gabriellemoran@gabrielles-air.home>
2024-01-01 11:27:52 +0000

    experimented with part 2

---
+++ dec1_part2.py
@@ -0,0 +1,30 @@
+
+inputs = []
+
+with open('dec1_part2_test_input.txt', 'r') as file:
+    for line in file:
+        inputs.append(line.strip())
+        
+print(inputs)
+nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+integers = []
+set = []
+for ele in inputs:
+    
+    for num in nums:
+        if num in ele:
+            print(num)
+            set.append(nums.index(num))
+            continue
+    print(set)
+    if len(set) == 1:
+        set.append(set[0])
+    integers.append(set)
+    
+    set = []
+print(integers)
+# for ele in nums:
+#     for line in inputs:
+#         if ele in line:
+#             print(ele)
+print("eles NOT IN CORRECT ORDER YET")
