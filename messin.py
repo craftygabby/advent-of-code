d8d1dda337f1e4ddee6764f0bbd19109b5518539
Gabrielle Moran <gabriellemoran@gabrielles-air.home>
2024-01-01 11:39:30 +0000

    messin 1/1/24

---
+++ messin.py
@@ -0,0 +1,35 @@
+
+# Messin with script for dec1 part2 1 Jan 24
+data = []
+with open('dec1_part2_test_input.txt', 'r') as file:
+    for line in file:
+        data.append(line.strip())
+#data = data.split()
+print(data)
+for line in data:
+    print(line)
+
+
+
+# nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+# count = 0
+# collect_digits = []
+# vals = []
+
+# for dig, num in enumerate(nums):
+#     if num in data[4]:
+#         collect_digits.append(num)
+# for i, num in enumerate(collect_digits):
+#     collect_digits[i] = nums.index(num)
+# # for ele in data:
+# #     for dig, num in enumerate(nums):
+# #         if num in ele:
+# #             print(dig, num)
+# #             collect_digits.append(dig)
+# #             continue
+# print(collect_digits)
+# vals.append(collect_digits)
+# print(vals)
+    
+
+        
